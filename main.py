73bed6f51baa1727ca713cdc0e0dec8774ba5fe3
SYLG <2330180633@qq.com>
2022-07-18 00:18:05 +0800

    add help and credits, add banner to several functions

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 from mttkinter import mtTkinter as tkinter
 import json,time,numpy as np
 import camera,chartLoader,music
-#from PIL import Image,ImageTk
-
-# def image_CV2ToTk(img):
-# 	image=Image.fromarray(np.uint8(cv2.cvtColor(img,cv2.COLOR_BGR2RGB)))
-# 	return ImageTk.PhotoImage(image=image)
 
 def start():
 	hideAll()# tkinter window would stuck here, reason unknown! (bug not fatal)
@@ -26,18 +21,38 @@ def start():
 	musicThread.join()
 	showMenu()
 
-def option():
+def options():
+	hideAll()
+	showOptions()
+	showBack()
+
+def help():
+	hideAll()
+	showHelp()
+	showBack()
+
+def credits():
+	hideAll()
+	showCredits()
+	showBack()
+
+def back():
 	hideAll()
 	showMenu()
-	showOption()
 
 win=tkinter.Tk()
 win.title('AIDDR')
 win.geometry('1400x800')
 
 startButton=tkinter.Button(win,command=start,text='Start')
-optionButton=tkinter.Button(win,command=option,text='Option')
+optionsButton=tkinter.Button(win,command=options,text='Options')
+helpButton=tkinter.Button(win,command=help,text='Help')
+creditsButton=tkinter.Button(win,command=credits,text='Credits')
 exitButton=tkinter.Button(win,command=lambda : win.destroy(),text='Exit')
+
+backButton=tkinter.Button(win,command=back,text='Back')
+
+optionsBanner=tkinter.Label(win,text='Options',font=('Arial', 15))
 
 volumeScale=tkinter.Scale(win,from_=0,to=100,orient=tkinter.HORIZONTAL)
 volumeScale.set(100)
@@ -47,31 +62,83 @@ offsetScale=tkinter.Scale(win,from_=-100,to=100,orient=tkinter.HORIZONTAL)
 offsetScale.set(0)
 offsetLabel=tkinter.Label(win,text='offset')
 
+helpBanner=tkinter.Label(win,text='Help of AIDDR',font=('Arial', 15))
+helpContent=tkinter.Label(win,text='''What is AIDDR?\n
+	DDR(Dance Dance Revolution) is a series of video games made by Konami
+	 in which players step on arrows on a large pad or mat to match the arrows on screen.
+	 The arrows are in time with the music.
+	 Because players are moving themselves along to the music,
+	 they look like they are dancing.\n
+	With Artificial Intelligence, we can now have fun at home.
+	 Instead of a large pad, all you need is a camera.
+	 Artificial Intelligence will judge your motion and score accordingly.\n
+	How to play AIDDR?\n
+	Stretch out your arms and legs as the arrows reach the bottom of the screen.
+	For example, if it's a top-left arrow, top means you should use your arm,
+	 and left means you should use your left arm/leg.
+	 So, in this case, just stretch out your left arm
+	 when that arrow reach the bottom of the screen.
+	''')
+
+creditsBanner=tkinter.Label(win,text='AIDDR\n@AI001-AIDDR team 2022',font=('Arial', 15))
+creditsContent=tkinter.Label(win,text='Team members:\nSYLG\ncatanduni\nderivative233\n\nSpecial thanks to:\nBig_True')
+
 def showMenu():
 	startButton.pack(anchor='ne',side=tkinter.TOP,padx=50,pady=20,expand=True)
-	optionButton.pack(anchor='ne',side=tkinter.TOP,padx=50,pady=20,expand=True)
+	optionsButton.pack(anchor='ne',side=tkinter.TOP,padx=50,pady=20,expand=True)
+	helpButton.pack(anchor='ne',side=tkinter.TOP,padx=50,pady=20,expand=True)
+	creditsButton.pack(anchor='ne',side=tkinter.TOP,padx=50,pady=20,expand=True)
 	exitButton.pack(anchor='ne',side=tkinter.TOP,padx=50,pady=20,expand=True)
 
 def hideMenu():
 	startButton.pack_forget()
-	optionButton.pack_forget()
+	optionsButton.pack_forget()
+	helpButton.pack_forget()
+	creditsButton.pack_forget()
 	exitButton.pack_forget()
 
-def showOption():
+def showOptions():
+	optionsBanner.pack(anchor='n',side=tkinter.TOP)
 	volumeScale.pack(anchor='s',side=tkinter.TOP)
 	volumeLabel.pack(anchor='s',side=tkinter.TOP)
 	offsetScale.pack(anchor='s',side=tkinter.TOP)
 	offsetLabel.pack(anchor='s',side=tkinter.TOP)
 
-def hideOption():
+def hideOptions():
+	optionsBanner.pack_forget()
 	volumeScale.pack_forget()
 	volumeLabel.pack_forget()
 	offsetScale.pack_forget()
 	offsetLabel.pack_forget()
 
+def showHelp():
+	helpBanner.pack(anchor='n',side=tkinter.TOP)
+	helpContent.pack(anchor='n',side=tkinter.TOP)
+
+def hideHelp():
+	helpBanner.pack_forget()
+	helpContent.pack_forget()
+
+def showCredits():
+	creditsBanner.pack(anchor='n',side=tkinter.TOP)
+	creditsContent.pack(anchor='n',side=tkinter.TOP)
+
+def hideCredits():
+	creditsBanner.pack_forget()
+	creditsContent.pack_forget()
+
+def showBack():
+	backButton.pack(anchor='ne',side=tkinter.BOTTOM,padx=50,pady=20)
+
+def hideBack():
+	backButton.pack_forget()
+
 def hideAll():
 	hideMenu()
-	hideOption()
+	hideOptions()
+	hideHelp()
+	hideCredits()
+	hideBack()
 
 showMenu()
 win.mainloop()
