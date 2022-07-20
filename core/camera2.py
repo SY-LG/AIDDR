01102180bf195a45adf35beeef03eb24d121701c
catanduni <102228205+catanduni@users.noreply.github.com>
2022-07-20 19:36:16 +0800

    Update camera2.py

--- core/camera2.py
+++ core/camera2.py
@@ -3,24 +3,29 @@ import json
 import threading
 import cv2
 import time
+import csv
 
 import sys
-sys.path.append('D:\learning\python\work\AIDDR-main\core')#这边是自己的judge所在文件夹位置
+sys.path.append('D:\learning\python\work\AIDDR-main\core')
 import judge
 
-class cameraThread(threading.Thread，startTime):
-	def __init__(self):
+class cameraThread(threading.Thread):
+	def __init__(self,startTime,path):
 		threading.Thread.__init__(self)
 		self.quit=False
+		self.startTime=startTime
+		self.path=path
 	def run(self):
 		# 设置以utf-8解码模式读取文件，encoding参数必须设置，否则默认以gbk模式读取文件，当文件中包含中文时，会报错
-		f = open("music.json", encoding="utf-8")#解析文件，还需要知道具体哪个音乐文件
+		f = open(self.path, encoding="utf-8")
 		file = json.load(f)
 		res = []
 		for i in file["notes"]:
 			res.append(i)
 		pd.DataFrame(res).to_csv("music.csv")
 		pos=0#指代第几行
+
+		results=[]
 
 		camera=cv2.VideoCapture(0)
 		cv2.namedWindow('camera')
@@ -33,32 +38,34 @@ class cameraThread(threading.Thread，startTime):
 			perfect=0
 			good=0
 			bad=0
+			miss=1
 			judged=0
 
-			while((time.perf_counter()-startTime)>=res[pos]-300 and (time.perf_counter()-startTime)<=res[pos][2]+300):#判断移动一部分到这里，这里是什么时候应该开始判断及结果
+			while((time.perf_counter()-self.startTime)>=res[pos]-300 and (time.perf_counter()-self.startTime)<=res[pos][2]+300):#判断移动一部分到这里，这里是什么时候应该开始判断及结果
 				judged=1
 				bad=judge.judgeFrame(frame,res[pos][1])
 				if bad :
 					break
-				while((time.perf_counter()-startTime)>=res[pos]-160 and (time.perf_counter()-startTime)<=res[pos][2]+160):
+				while((time.perf_counter()-self.startTime)>=res[pos]-160 and (time.perf_counter()-self.startTime)<=res[pos][2]+160):
 					good =judge.judgeFrame(frame,res[pos][1])
 					if good :
 						break
-					while((time.perf_counter()-startTime)>=res[pos]-80 and (time.perf_counter()-startTime)<=res[pos][2]+80):
+					while((time.perf_counter()-self.startTime)>=res[pos]-80 and (time.perf_counter()-self.startTime)<=res[pos][2]+80):
 						perfect=judge.judgeFrame(frame,res[pos][1])
 						if perfect:
 							break
+
 			if judged:
 				pos+=1
-				result=0
-				if bad:
-					result=1
-				elif good:
-					resukt=2
-				elif perfect:
-					result=3
-			#可以另外定义一个函数传递result
-			#0:miss,1:bad,2:good,3:perfect
+				score=perfect+good+bad+1
+				miss=not (perfect or good or bad)
+				results.append = [{'score':score,'perfect':perfect,'good':good,'bad':bad,'miss':miss}]
+
+		with open ('results.csv','w',encoding='utf-8') as fp:
+			writer =csv.DictWriter(fp,['score','perfect','good','bad','miss'])
+			writer.writeheader()
+			writer.writerows(results)
 
 		camera.release()
 		cv2.destroyAllWindows()
+
